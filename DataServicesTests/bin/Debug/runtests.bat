cd67994a8921330812f383a028cca2ac0d91d55c
Roger Pence <roger.pence@gmail.com>
2014-09-20 16:06:23 -0500

    Major refactor--use interface for output

--- DataServicesTests/bin/Debug/runtests.bat
+++ DataServicesTests/bin/Debug/runtests.bat
@@ -1,5 +1,6 @@
 @echo off
+del results.trx
 set result=At least some tests failed.
-mstest /testcontainer:dataservicestests.dll
+mstest /testcontainer:dataservicestests.dll /resultsfile:results.trx
 if %errorlevel% equ 0 set result=All tests passed.
 echo %result%
